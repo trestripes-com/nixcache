b69f2c21ae39c1e675b5b74041e1ff6af8512a8d
Martin Toman <toman.martin@live.com>
2023-05-26 13:34:22 -0700

    Setup main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+
+use nixcache::{run_api_server, config};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dump_version();
+
+    let config = config::load().await?;
+
+    run_api_server(opts.listen, config).await?;
+
+    Ok(())
+}
+
+fn dump_version() {
+    #[cfg(debug_assertions)]
+    eprintln!("Nixcache {} (debug)", env!("CARGO_PKG_VERSION"));
+    #[cfg(not(debug_assertions))]
+    eprintln!("Nixcache {} (release)", env!("CARGO_PKG_VERSION"));
 }
